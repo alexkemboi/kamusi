bfaa8070553fb47ba1319ffe095cd1636c9571ed
Alex Kemboi <akemboi@KWFTBANK.INT>
2023-03-07 16:07:11 +0300

    added a function

--- kamusi.js
+++ kamusi.js
@@ -1,8 +1,9 @@
 let words = ['banana', 'apple', 'orange', 'grape', 'cherry'];
-
+let students=['alex','pius','zack','mary','john'];
 // Sort the array in reverse alphabetical order
-words.sort(function(a, b) {
-  return b.localeCompare(a);
-});
-
-console.log(words);
+function sortValues(values){
+  
+const sortedValues= values.sort();
+return sortedValues;
+}
+console.log(sortValues(students));
