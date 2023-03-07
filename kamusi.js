33ea20e026c3e0fd814534b9bd2b5abcd4f1b861
alexkemboi4127 <alexkemboi97@yahoo.com>
2023-03-07 23:06:56 +0300

    added a day switch statement

--- kamusi.js
+++ kamusi.js
@@ -7,3 +7,35 @@ const sortedValues= values.sort();
 return sortedValues;
 }
 console.log(sortValues(students));
+
+//switch statement
+let day = 5;
+let dayName;
+
+switch (day) {
+  case 0:
+    dayName = "Sunday";
+    break;
+  case 1:
+    dayName = "Monday";
+    break;
+  case 2:
+    dayName = "Tuesday";
+    break;
+  case 3:
+    dayName = "Wednesday";
+    break;
+  case 4:
+    dayName = "Thursday";
+    break;
+  case 5:
+    dayName = "Friday";
+    break;
+  case 6:
+    dayName = "Saturday";
+    break;
+  default:
+    dayName = "Unknown";
+}
+
+console.log("Today is " + dayName);
